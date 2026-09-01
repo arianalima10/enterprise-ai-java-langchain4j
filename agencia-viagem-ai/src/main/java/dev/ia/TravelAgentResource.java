3be46b043466982625ede53797cd2b107f26beed
arianalima10 <ariana.crodrigues@gmail.com>
2026-08-31 23:00:08 -0300

    Gerenciamento de estado(status) e Validação(iniciando com segurança).

--- agencia-viagem-ai/src/main/java/dev/ia/TravelAgentResource.java
+++ agencia-viagem-ai/src/main/java/dev/ia/TravelAgentResource.java
@@ -1,10 +1,7 @@
 package dev.ia;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/travel")
@@ -17,8 +14,18 @@ public class TravelAgentResource {
     @POST
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String ask(String question) {
-        return expert.chat("session-123", question);
-        //return assistant.chat(question); Usado com EasyRAG
+    public String ask(String question, @HeaderParam("X-User-Name") String userName) {
+        if (userName != null && !userName.isEmpty()) {
+            try {
+                SecurityContext.setCurrentUser(userName);
+                return expert.chat(userName, question); // Usar userName como memoryId
+                //return expert.chat("session-123", question); Usado com RAG VECTOR DataBase
+                //return assistant.chat(question); Usado com EasyRAG
+            } finally {
+                SecurityContext.clear();
+            }
+        } else {
+            return "Usuário precisa estar autenticado!";
+        }
     }
 }
